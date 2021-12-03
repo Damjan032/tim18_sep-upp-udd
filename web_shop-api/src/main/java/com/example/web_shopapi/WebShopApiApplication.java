d64c9581b82b1874022b322cc37fb546353d38fb
Damjan Manojlović <34902687+Damjan032@users.noreply.github.com>
2021-12-03 20:15:50 +0100

    Iniit web shop api and ui

---
+++ web_shop-api/src/main/java/com/example/web_shopapi/WebShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_shopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShopApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebShopApiApplication.class, args);
+    }
+
+}
---
+++ web_shop-api/src/test/java/com/example/web_shopapi/WebShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_shopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShopApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
