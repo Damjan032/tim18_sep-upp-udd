ae20e436175f92cb0d78da6e489a3b9aef51bdbc
Makim1998 <mmijatovic32@gmail.com>
2021-12-11 12:19:54 +0100

    redirekcija na front pspa prilikom kupovine

--- web_shop-ui/src/pages/Basket.vue
+++ web_shop-ui/src/pages/Basket.vue
@@ -4,14 +4,24 @@
        justify="center">
     <basket-table/>
   </div>
+  <v-btn color="primary" @click="redirectToPsp()">
+    Buy
+  </v-btn>
   </v-container>
 </template>
 
 <script>
 import BasketTable from "@/components/WebShopItems/BasketTable";
+import {pspFrontPath} from "@/utils/paths";
+
 export default {
   name: "Basket",
-  components: {BasketTable}
+  components: {BasketTable},
+  methods: {
+    redirectToPsp() {
+        window.location.href = pspFrontPath
+    }
+  }
 }
 </script>
 
--- web_shop-ui/src/utils/paths.js
+++ web_shop-ui/src/utils/paths.js
@@ -2,6 +2,8 @@ export const homePath = '/';
 
 export const basketPath = '/basket';
 
+export const pspFrontPath = 'http://localhost:4201';
+
 export const certificateDetails = '/addCertificate/:alias';
 
 export const createCertificate = "/createCertificate";
