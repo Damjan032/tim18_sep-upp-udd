b97f75cc851a914a313279479765c949cd648cb5
Makim1998 <mmijatovic32@gmail.com>
2022-01-31 18:32:45 +0100

    dodate skripte za popunu baza na psp, banci, i servisu za placanje karticom

---
+++ payment-service-provider/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO `payment_types` VALUES ('4028808c7dac61d3017dac9517690001','CARD');
+
+INSERT INTO `user` VALUES ('2c9650817da05f82017da06111cd0000',true,'djkristinaxx1@gmail.com','$2a$10$hlxjtbXodipVY8VQVptNZe3NluB8gchS32ba74GFx0wYCqC.lPIre',false,'ROLE_WEB_SHOP_ADMIN','4028808c7dac61d3017dac9517690001');
+
+INSERT INTO `web_shop` VALUES ('4028808c7dac61d3017dac9517690001','Subshop','error-url 2','failure-url 2','success-url 2',NULL);
+
